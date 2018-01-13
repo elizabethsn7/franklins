b6a14c07993d296265565a6f6574a800f9cd1c17
Liz Kipp <liz@elizabethkipp.com>
2018-01-13 14:03:21 -0600

    changed nav bar, added cols

--- js/main-min.js
+++ js/main-min.js
@@ -1,1 +1,1 @@
-document.querySelector("html").onclick=function(){alert("Ouch! Stop poking me!")};
+$(window).scroll(function(){$(".navbar").offset().top>100?($("#custom-nav").addClass("affix"),$(".navbar-fixed-top").addClass("top-nav-collapse"),$(".navbar-brand img").attr("src","img/franklinsHomeLogoBlack.png")):($("#custom-nav").removeClass("affix"),$(".navbar-fixed-top").removeClass("top-nav-collapse"),$(".navbar-brand img").attr("src","img/franklinsHomeLogoWhite.png"))});
---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(window).scroll(function() {
+    if ($(".navbar").offset().top > 100) {
+        $('#custom-nav').addClass('affix');
+        $(".navbar-fixed-top").addClass("top-nav-collapse");
+        $('.navbar-brand img').attr('src','img/franklinsHomeLogoBlack.png'); //change src
+    } else {
+        $('#custom-nav').removeClass('affix');
+        $(".navbar-fixed-top").removeClass("top-nav-collapse");
+        $('.navbar-brand img').attr('src','img/franklinsHomeLogoWhite.png')
+    }
+});
