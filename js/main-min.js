e0faebbf05aecc52849958328ab23ef7862631e2
Elizabeth Kipp <liz@elizabethkipp.com>
2018-01-29 22:06:04 -0600

    worked on private events page

--- js/main-min.js
+++
@@ -1,1 +0,0 @@
-$(window).scroll(function(){$(".navbar").offset().top>100?($("#custom-nav").addClass("affix"),$(".navbar-fixed-top").addClass("top-nav-collapse"),$(".navbar-brand img").attr("src","img/franklinsHomeLogoBlack.png")):($("#custom-nav").removeClass("affix"),$(".navbar-fixed-top").removeClass("top-nav-collapse"),$(".navbar-brand img").attr("src","img/franklinsHomeLogoWhite.png"))});
